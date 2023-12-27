71e20e01a1ae68c50b07075274d82e79e1a96418
Yusuf <holla2specialyusuf@gmailcom>
2023-12-27 20:39:39 +0100

    working commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Add any JavaScript functionality here if needed
+document.getElementById("examForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // You can add code here to handle form submission, validation, or data processing
+  });
+  function clearForm() {
+    document.getElementById('myForm').reset();
+  }
